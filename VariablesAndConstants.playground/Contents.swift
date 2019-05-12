336de74ff466cebcef43ac7ba209d445e81cb531
JuyelRana <mjuyelrana@gmail.com>
2019-05-12 12:40:55 +0600

    01. Introduction to swift-Variables and Constant

---
+++ VariablesAndConstants.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var name = "Juyel Rana"  // Initialize Variable name
+print(name)
+
+name = "Md Juyel Rana"  // Reinitialize variable name
+print(name)
+
+//var name = "Juyel"  // Impossible to declare same name as variable
+
+let address = "Ramjan Para, Nordash, Bagmara, Rajshahi"  // Initialize constant address that value will never change
+print(address)
+
+//address = "Khulna, Fox House" // can not assign to value in constant
+
+
+
+
