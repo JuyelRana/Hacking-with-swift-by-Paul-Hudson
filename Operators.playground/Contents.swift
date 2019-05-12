9cd2c004fd893432dbb3470cc7bf69981e15d720
JuyelRana <mjuyelrana@gmail.com>
2019-05-12 16:52:06 +0600

    03.Introduction to Swift-Operators

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+var a = 10
+
+a = a + 1  // Addition Operator
+a = a - 1  // Substraction Operator
+a = a * a
+
+var b = 10
+b += 10 // Add AND Assignment operator
+b -= 10 // Subtract AND Assignment operator
+
+var aa = 1.1
+var bb = 2.2
+var cc = aa + bb
+
+var name1 = "Juyel"
+var name2 = "Rana"
+var both = name1 + " " + name2
+
+print(cc > 3)  //Greater than operator check left value is greater than the right value
+print(cc >= 3) //Greater than And Equal to operator, check left value is greater than or equal to the right value.
+print(cc > 4) //Greater than operator
+print(cc < 4) //Less than operator
+
+var name = "Juyel Rana"
+name == "Md Juyel Rana"
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
+
+
+
+
+
+
+
+
