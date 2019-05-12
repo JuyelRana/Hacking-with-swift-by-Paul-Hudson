c23f7eac3959d08d11ea5fa2294306b48b3027de
JuyelRana <mjuyelrana@gmail.com>
2019-05-12 14:17:44 +0600

    02.Introduction to Swift- Types of data

--- TypesOfData.playground/Contents.swift
+++ TypesOfData.playground/Contents.swift
@@ -6,11 +6,31 @@ var age = 25  // Int type data
 // name = 45   // Impossible to assign Int to a String variable
 // age = "Juyel Rana" // Impossible to assign String to an Int Variable
 
-var address: String
-var phone: Int
+var address: String //Declare String variable
+var phone: Int // Declare Int Variable
 
-address = "Ramajan Para"
-phone = 01726903951
+address = "Ramajan Para" //Assign value to String Variable
+phone = 01726903951 //Assign value to Int Variable
+
+var latitude: Double //Declare Double variable
+latitude = 36.166678 //Assign double value to latitude
+
+var longitude: Float  //Declare Float variable
+longitude = -1322.6754353 //Assign Float value to longitude, Incase of Float accuracy not good but using Double get better accuracy
+
+var stayOutTooLate: Bool //Declare Boolean variable
+stayOutTooLate = true //Assign Boolean value to stayOutTooLate variable
+
+var nothingInBrain: Bool //Declare Boolean variable
+nothingInBrain = true //Assign Boolean value to nothingInBrain variable
+
+var missABeat = false //Boolean type data
+
+
+
+
+
+
 
 
 
