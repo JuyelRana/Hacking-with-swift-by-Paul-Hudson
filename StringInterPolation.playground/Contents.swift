ec1bcc26e800878e3254a9448b3c3113cd007390
JuyelRana <mjuyelrana@gmail.com>
2019-05-12 17:26:48 +0600

    03.Introduction to Swift-String Interpolation.

---
+++ StringInterPolation.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var name = "Juyel Rana"
+var age = 25
+var latitude = 36.166667
+
+print("Your name is \(name), Your age is \(age), and your latitude is \(latitude).")
+
+print("Your age is \(age) years old. In another \(age) years will be \(age * 2)")
+
+
+
+
