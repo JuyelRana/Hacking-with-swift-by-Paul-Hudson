1fb18a9c38380e99988b33dfc0be41805699a76e
JuyelRana <mjuyelrana@gmail.com>
2019-05-12 13:05:03 +0600

    Test

---
+++ TypesOfData.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var name = "Juyel Rana"  // String type data
+var age = 25  // Int type data
+
+// name = 45   // Impossible to assign Int to a String variable
+// age = "Juyel Rana" // Impossible to assign String to an Int Variable
+
+var address: String
+var phone: Int
+
+address = "Ramajan Para"
+phone = 01726903951
+
+
+
