82496b34e83cc1e8eb3eeb373ddef9301b25baf1
JuyelRana <mjuyelrana@gmail.com>
2019-05-15 17:13:33 +0600

    07.Introduction to Swift- Dictionaries

---
+++ Dictionaries.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var person = [
+    "firstname": "Mohammad",
+    "middlename": "Juyel",
+    "lastname": "Rana",
+    "email": "mjuyelrana@gmail.com",
+    "phone": "01726903951",
+    "age": "23",
+    "sex": "Male",
+    "nationality": "Bangladeshi",
+    "religion": "Mushlim",
+    "website": "www.juyelrana.com"
+]
+
+print(person["middlename"]!)
+print(person["lastname"]!)
+print(person["website"]!)
+
